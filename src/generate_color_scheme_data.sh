eb3cbe820501076a69ffc42bf19b869e5a355d4f
Robin Gagnon <me@reobin.dev>
2020-08-17 13:14:31 -0400

    feat: Add option for color scheme name

--- src/generate_color_scheme_data.sh
+++ src/generate_color_scheme_data.sh
@@ -4,6 +4,10 @@ git clone https://github.com/sheerun/vim-polyglot ~/.vim/pack/vim-polyglot/start
 
 owner_name="$1"
 name="$2"
+color_scheme_name="$3"
+if [ -z $color_scheme_name ]; then
+  color_scheme_name=$name
+fi
 
 # creating the vim plugin directory
 mkdir -p ~/.vim/pack/$name/start/$name
@@ -14,7 +18,7 @@ git clone --depth 1 \
 
 cat set_termguicolors.vim > ~/.vimrc
 
-echo "colorscheme $name" >> ~/.vimrc
+echo "colorscheme $color_scheme_name" >> ~/.vimrc
 
 cat vcspg.vim >> ~/.vimrc
 
